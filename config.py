a078a7f2a5540a4a5ef1b2b2d40b5ccc840edb0c
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-03-04 17:13:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ import logging
 class Config:
   
 #Your API HASH from my.telegram.org    
-    API_ID = int(os.environ.get("API_ID", "18576653"))
+    API_ID = int(os.environ.get("API_ID", "29409646"))
 #Your API HASH from my.telegram.org      
-    API_HASH = os.environ.get("API_HASH", "d29fa01d174ec2ac0d5bd415f052d173")
+    API_HASH = os.environ.get("API_HASH", "a69d0340a520c1913c517bea143a3de7")
 #BOT TOKEN: @Botfather on telegram    
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5647123835:AAHN6PUFsVpCFkxYuSdHlX4iW_Wc8Nuo7IU")
 #Public channel username without '@'. Don't forget to add bot in channel as administrator.    
@@ -18,7 +18,7 @@ class Config:
 #Owner user id   
     AUTH  = os.environ.get("OWNER_ID", "5143506371")
 #Pyrogram string session using (https://replit.com/@KindKobra/Pyrogram-String-Gen?v=1)
-    SESSION = os.environ.get("SESSION", "AQCV8ShRKtiHlOomLaPBDfs2MQm6y9vnVbBwNtkqTp-fZj8RTw3nCbwsk9tw-V6KGztX9sfj8uxAwb0OpvEx__CBjO0DGnSuNv05xYbTikMrqe_HuPyhsCYVsEw9F32KKwKAxKE8Uf7nhAUvwMOqRqCmiXc44Z6BiuaYc91zNm6AeHox_-YzeXF6frc-bfPGadZt9iwWOvNfS4Rdyo21_SmpkDH7iZ7ydVtk_kLn4c-pHtK1iwU_HNW2VXskeNVv2wYSyhQXMwQRvY-jTusB5z2etzvAr4Vhm4H4I4jAnjxABaDeWULzW9Z5nAfbvyxIEfbwJyEa2pE8HDNURM_ZkmzqAAAAATKTrcMA")
+    SESSION = os.environ.get("SESSION", "AQAYE4IFXO0SjiKDKlwhmYLaTIFCkNq1LHENlT8DExAIphz0ibtjpupxNTKqVNei10_Z4OsHpSJinRPaq4dGTDmEqzPyV1l78jBNKgu1Yv5wd-VddiczExq8TQqfgO3K4pn8zf8PSbItUAkmNSsYN9daJeQW3xW1H_Q9S22SIgvXu902r4WyeYo96GNMyz8A4MGO1prt0V6PwnANctf94_YQTp95Xz2JkQ6J4peJNic8pKZG5qbt1ln3QoRFVVkELyJDZBZXx2iWZmDul-gyVsTPDZufqmEKvZmWl4tYxnybZPRBIvsjBd22k4ifQfScHs19mgrBhiNHooXMVrJUk5QaAAAAAV6vDl0A")
   
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
